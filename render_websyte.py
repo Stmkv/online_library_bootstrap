08cbf039a062a4a842d6f32b9fd312cca76d89d8
Stepan Makarov <st.mkv03@gmail.com>
2024-10-11 12:28:50 +0300

    feat(site): create start page

---
+++ render_websyte.py
@@ -0,0 +1,19 @@
+import json
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+with open(r"books\books.json", "r") as file:
+    books = file.read()
+
+books_description = json.loads(books)
+
+env = Environment(
+    loader=FileSystemLoader("."), autoescape=select_autoescape(["html", "xml"])
+)
+
+template = env.get_template("template.html")
+
+render_page = template.render(books=books_description)
+
+with open("index.html", "w", encoding="utf8") as file:
+    file.write(render_page)
