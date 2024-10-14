4334b631302950954a616c9802932fbe0378899b
Stepan Makarov <st.mkv03@gmail.com>
2024-10-14 12:58:54 +0300

    feat(site):Integrate livereload.

--- render_websyte.py
+++ render_websyte.py
@@ -1,19 +1,32 @@
 import json
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from livereload import Server
 
-with open(r"books\books.json", "r") as file:
-    books = file.read()
 
-books_description = json.loads(books)
+def on_reload():
+    with open(r"books\books.json", "r") as file:
+        books = file.read()
 
-env = Environment(
-    loader=FileSystemLoader("."), autoescape=select_autoescape(["html", "xml"])
-)
+    books_description = json.loads(books)
 
-template = env.get_template("template.html")
+    env = Environment(
+        loader=FileSystemLoader("."), autoescape=select_autoescape(["html", "xml"])
+    )
+    template = env.get_template("template.html")
 
-render_page = template.render(books=books_description)
+    render_page = template.render(books=books_description)
 
-with open("index.html", "w", encoding="utf8") as file:
-    file.write(render_page)
+    with open("index.html", "w", encoding="utf8") as file:
+        file.write(render_page)
+
+
+def main():
+    on_reload()
+    server = Server()
+    server.watch("template.html", on_reload)
+    server.serve(root=".")
+
+
+if __name__ == "__main__":
+    main()
