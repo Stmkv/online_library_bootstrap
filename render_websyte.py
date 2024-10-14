a2b6c2536ee0cb57df3128d1c22c34abb5dcfc4e
Stepan Makarov <st.mkv03@gmail.com>
2024-10-14 15:04:56 +0300

    feat(site): Split books list by pages.

--- render_websyte.py
+++ render_websyte.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
@@ -14,11 +15,19 @@ def on_reload():
     with open(r"books\books.json", "r", encoding="utf-8") as file:
         books_description = json.load(file)
 
-    books_descriptions = list(chunked(books_description, 2))
-
-    render_page = template.render(books=books_descriptions)
-    with open("index.html", "w", encoding="utf8") as file:
-        file.write(render_page)
+    books_descriptions = list(chunked(books_description, 20))
+    pages_amount = len(books_descriptions)
+    for page_number, books_on_page in enumerate(books_descriptions, start=1):
+        render_page = template.render(
+            books=list(chunked(books_on_page, 2)),
+            pages_amount=pages_amount,
+            page_number=page_number,
+        )
+        os.makedirs("pages/", exist_ok=True)
+        with open(
+            os.path.join("pages/", f"index{page_number}.html"), "w", encoding="utf8"
+        ) as file:
+            file.write(render_page)
 
 
 def main():
